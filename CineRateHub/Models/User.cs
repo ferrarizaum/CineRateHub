a8fc1f6ea78496dd20c1180b01799180bcea3a47
José Ferrari <licoverpa321@gmail.com>
2024-03-04 22:53:42 -0300

    Adding Models

---
+++ CineRateHub/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace CineRateHub.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public string? Job { get; set; }
+    }
+}
