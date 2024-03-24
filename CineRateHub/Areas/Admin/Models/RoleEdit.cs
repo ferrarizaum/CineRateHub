894ae9315ce794cee00673d4868aa07bd71e0af5
José Ferrari <licoverpa321@gmail.com>
2024-03-23 21:18:09 -0300

    Reminder

--- CineRateHub/Areas/Admin/Models/RoleEdit.cs
+++ CineRateHub/Areas/Admin/Models/RoleEdit.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 
 namespace CineRateHub.Areas.Admin.Models
 {
+    //not used, just added so Areas.Admin.Models would be enabled
     public class RoleEdit
     {
         public IdentityRole? Role { get; set; }
